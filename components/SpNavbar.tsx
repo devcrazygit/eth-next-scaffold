46c6501af786b6630e2953f5a9a5ea4aced7ad90
devcrazygit <devcrazy@hotmail.com>
2022-04-20 01:58:02 +0800

    initial commit

---
+++ components/SpNavbar.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+
+const SpNavbar = () => {
+    return (
+        <div className="popup-mobile-menu one-page-vavigation-popup">
+            <div className="inner">
+                <div className="header-top">
+                    <div className="logo logo-custom-css">
+                        <a className="logo-light" href="index.html">
+                            <Image src="/assets/images/logo/logo-white.png" alt="nft-logo" />
+                        </a>
+                        <a className="logo-dark" href="index.html">
+                            <Image src="/assets/images/logo/logo-dark.png" alt="nft-logo" />
+                        </a>
+                    </div>
+                    <div className="close-menu">
+                        <button className="close-button">
+                            <i className="feather-x"></i>
+                        </button>
+                    </div>
+                </div>
+                <nav>
+                    <ul className="mainmenu" id="navbar-example2">
+                        <li className="nav-item"><a className="nav-link smoth-animation" href="#list-item-1">Home</a></li>
+                        <li className="nav-item"><a className="nav-link smoth-animation" href="#list-item-2">Live Auction</a></li>
+                        <li className="nav-item"><a className="nav-link smoth-animation" href="#list-item-3">Newest Item</a></li>
+                        <li className="nav-item"><a className="nav-link smoth-animation" href="#list-item-4">Explore Product</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    );
+}
+
+export default SpNavbar;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
+import '../styles/feature/feature.css';
+import '../styles/vendor/bootstrap.min.css';
+import '../styles/vendor/nice-select.css';
+import '../styles/vendor/slick-theme.css';
+import '../styles/vendor/slick.css';
+
+import '../styles/style.scss';
+
+
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
